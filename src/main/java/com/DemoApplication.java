d742d5cba8dc6dfb2d8faeda339cfc94d1692d83
gpcgpan <guopan2011@126.com>
2018-09-13 10:59:06 +0800

    test

---
+++ src/main/java/com/DemoApplication.java
@@ -0,0 +1,25 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * author: 小殊
+ * create: 2018/9/13 上午9:10
+ */
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String args[]) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @RequestMapping("hello")
+    public String hello(){
+        return "hello world";
+    }
+}
+
+
